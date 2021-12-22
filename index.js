2bbfb037495203e3a766daeecefe6ed2856c7e27
Lohen <17508663+lohenyumnam@users.noreply.github.com>
2021-12-22 12:05:31 +0530

    Santa magic

--- index.js
+++ index.js
@@ -2,7 +2,4 @@ const name = "Toro";
 
 console.log(`Hello ${name}`);
 
-// I am changing this file in master branch
 console.log(`We are in dev branch`);
-
-// I am changing this file in master branch
